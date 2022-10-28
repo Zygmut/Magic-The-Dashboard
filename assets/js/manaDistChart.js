fb6c20f92b376b53be8a9d41aaf069a9473be77e
Rubén Palmer <40397192+Zygmut@users.noreply.github.com>
2022-10-28 21:43:20 +0200

    ColorPalette integration

--- assets/js/manaDistChart.js
+++ assets/js/manaDistChart.js
@@ -39,32 +39,33 @@ function manaDistChart(data) {
           {
             name: "Black",
             y: data.B,
-            color: "#101010",
+            color: colorPalette.B,
           },
           {
             name: "Blue",
             y: data.U,
-            color: "#0280B7",
+            color: colorPalette.U,
           },
           {
             name: "White",
             y: data.W,
-            color: "#F3F1DD",
+            color: colorPalette.W,
           },
           {
             name: "Green",
             y: data.G,
-            color: "#017C45",
+            color: colorPalette.G,
           },
           {
             name: "Red",
             y: data.R,
-            color: "#F95454",
+            color: colorPalette.R,
           },
           {
             name: "None",
             y: data.N,
-            color: "#828282",
+            color: colorPalette.N,
+
           },
         ],
       },
